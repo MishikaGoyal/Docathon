65acdfcc32d01918c0ba8948ce7f4e105c9a026b
sj11105 <snehajain11105@gmail.com>
2025-05-12 21:43:00 +0530

    feat:Added login page and improved design

---
+++ frontend/src/app/auth/login/page.tsx
@@ -0,0 +1,31 @@
+import { LoginForm } from "@/components/login-form";
+import { GalleryVerticalEnd } from "lucide-react";
+
+export default function LoginPage() {
+  return (
+    <div className="grid min-h-dvh lg:grid-cols-2">
+      <div className="bg-muted relative hidden lg:block mt-6ml-4">
+        <img
+          src="https://i.pinimg.com/originals/b8/23/e3/b823e38cc01fdb9278b6f7faa2feda6d.gif"
+          alt="Image"
+          className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+      <div className="flex flex-col gap-4 p-6 md:p-10">
+        <div className="flex justify-center gap-2 md:justify-start">
+          <a href="/" className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Docathon
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
